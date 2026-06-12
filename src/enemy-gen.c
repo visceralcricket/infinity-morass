7142c7b18060ea35e6e5031af78fffb5fbb8481f
Jonathan Urrutia <jonathan.urrutia.t@mail.pucv.cl>
2026-06-11 23:33:28 -0400

    Finalizacion de generacion de enemigos comunes y enemigos de tipo Jefe

--- src/enemy-gen.c
+++ src/enemy-gen.c
@@ -21,6 +21,11 @@
 #define MAX_LORE_LENGTH 256
 #define N 30 // <- Maze's fixed size
 
+// crear objetos
+
+
+
+
 Enemy* generateEnemy(char name[MAX_USERNAME])
 {
     Enemy *enemy = (Enemy*) malloc(sizeof(Enemy));
@@ -42,66 +47,82 @@ Enemy* generateEnemy(char name[MAX_USERNAME])
 
 void generateStatsCommonEnemy(Enemy* enemy, int difficulty)
 {
-    float mult;
-
-
-    switch(difficulty) 
-    {
-    case 1: mult = 0.80f; break;
-
-    case 2: mult = 1.0f;  break;
-
-    case 3: mult = 2.0f;  break;
-    
-    default: mult = 1.0f; break;
-    }              mult = 2.0f;
-
-
-
     if(strcmp(enemy->enemyName, "Zombi") == 0) {
-        enemy->combatStats.attack  = 2;
-        enemy->combatStats.defense = 4;
-        enemy->combatStats.maxHp   = 12;
+        enemy->combatStats.attack    = 2  * difficulty;
+        enemy->combatStats.defense   = 4  * difficulty;
+        enemy->combatStats.maxHp     = 12 * difficulty;
         enemy->combatStats.currentHp = enemy->combatStats.maxHp;
-        enemy->combatStats.speed   = 2;
+        enemy->combatStats.speed     = 2  * difficulty;
     }
-    else if(strcmp(enemy->enemyName, "Esquelo") == 0) {
-        enemy->combatStats.attack  = 4;
-        enemy->combatStats.defense = 0;
-        enemy->combatStats.maxHp   = 8;
+    else if(strcmp(enemy->enemyName, "Esqueleto") == 0) {
+        enemy->combatStats.attack    = 4 * difficulty;
+        enemy->combatStats.defense   = 0 * difficulty;
+        enemy->combatStats.maxHp     = 8 * difficulty;
         enemy->combatStats.currentHp = enemy->combatStats.maxHp;
-        enemy->combatStats.speed   = 5;
+        enemy->combatStats.speed     = 5 * difficulty;
     }
     else if(strcmp(enemy->enemyName, "Slime") == 0) {
-        enemy->combatStats.attack  = 1;
-        enemy->combatStats.defense = 2;
-        enemy->combatStats.maxHp   = 35;
+        enemy->combatStats.attack    = 1  * difficulty;
+        enemy->combatStats.defense   = 2  * difficulty;
+        enemy->combatStats.maxHp     = 35 * difficulty;
         enemy->combatStats.currentHp = enemy->combatStats.maxHp;
-        enemy->combatStats.speed   = 1;
+        enemy->combatStats.speed     = 1  * difficulty;
     }
     else if(strcmp(enemy->enemyName, "Demonio") == 0) {
-        enemy->combatStats.attack  = 6;
-        enemy->combatStats.defense = 6;
-        enemy->combatStats.maxHp   = 6;
+        enemy->combatStats.attack    = 6 * difficulty;
+        enemy->combatStats.defense   = 6 * difficulty;
+        enemy->combatStats.maxHp     = 6 * difficulty;
         enemy->combatStats.currentHp = enemy->combatStats.maxHp;
-        enemy->combatStats.speed   = 6;
+        enemy->combatStats.speed     = 6 * difficulty;
     }
     else if(strcmp(enemy->enemyName, "Sucubo") == 0) {
-        enemy->combatStats.attack  = 10;
-        enemy->combatStats.defense = 0;
-        enemy->combatStats.maxHp   = 5;
+        enemy->combatStats.attack    = 10 * difficulty;
+        enemy->combatStats.defense   = 0  * difficulty;
+        enemy->combatStats.maxHp     = 5  * difficulty;
         enemy->combatStats.currentHp = enemy->combatStats.maxHp;
-        enemy->combatStats.speed   = 10;
+        enemy->combatStats.speed     = 10 * difficulty;
     }
     else if(strcmp(enemy->enemyName, "Goblin") == 0) {
-        enemy->combatStats.attack  = 2;
-        enemy->combatStats.defense = 2;
-        enemy->combatStats.maxHp   = 2;
+        enemy->combatStats.attack    = 2 * difficulty;
+        enemy->combatStats.defense   = 2 * difficulty;
+        enemy->combatStats.maxHp     = 2 * difficulty;
         enemy->combatStats.currentHp = enemy->combatStats.maxHp;
-        enemy->combatStats.speed   = 2;
+        enemy->combatStats.speed     = 2 * difficulty;
+    }
+}
+
+void generateStatsBossEnemy(Enemy* enemy, int difficulty)
+{
+    if(strcmp(enemy->enemyName, "Bandolero") == 0) {
+        enemy->combatStats.attack    = 12 * difficulty;
+        enemy->combatStats.defense   = 4 * difficulty;
+        enemy->combatStats.maxHp     = 18 * difficulty;
+        enemy->combatStats.currentHp = enemy->combatStats.maxHp;
+        enemy->combatStats.speed     = 14 * difficulty;
+    }
+    else if(strcmp(enemy->enemyName, "Aberracion Cosmica") == 0) {
+        enemy->combatStats.attack    = 9 * difficulty;
+        enemy->combatStats.defense   = 13 * difficulty;
+        enemy->combatStats.maxHp     = 40 * difficulty;
+        enemy->combatStats.currentHp = enemy->combatStats.maxHp;
+        enemy->combatStats.speed     = 4 * difficulty;
+    }
+    else if(strcmp(enemy->enemyName, "Robot Antiguo") == 0) { // palabras de androide 16
+        enemy->combatStats.attack    = 14 * difficulty;
+        enemy->combatStats.defense   = 6 * difficulty;
+        enemy->combatStats.maxHp     = 30 * difficulty;
+        enemy->combatStats.currentHp = enemy->combatStats.maxHp;
+        enemy->combatStats.speed     = 1 * difficulty;
+    }
+    else if(strcmp(enemy->enemyName, "Jefe de carrera") == 0) { // te manda a repetir el semestre, fallas 2 muerte instantanea, si aciertas 5 pasas
+        enemy->combatStats.attack    = 99 * difficulty;
+        enemy->combatStats.defense   = 99 * difficulty;
+        enemy->combatStats.maxHp     = 99* difficulty;
+        enemy->combatStats.currentHp = enemy->combatStats.maxHp;
+        enemy->combatStats.speed     = 99 * difficulty;
     }
 }
 
 
-// dificultad 1-3
-// ["Zombi", "Esquelo", "Slime", "Demonio", "Sucubo", "Goblin"]
+
+
